6b8dfcb9892417f81b33f18642fd7c29cf7de518
Komal <baggukomal126@gmail.com>
2024-07-01 00:23:52 +0530

    coomit

--- script.js
+++ script.js
@@ -11,4 +11,12 @@
             event.currentTarget.classList.add("active");
             document.getElementById(titlename).classList.add("active-contents");
         }
+        var sidemenu=document.getElementById("sidemenu");
+        function openmenu(){
+            sidemenu.style.right="0";
+        }
+        function closemenu(){
+            sidemenu.style.right="-200px";
+        }
+    
     
